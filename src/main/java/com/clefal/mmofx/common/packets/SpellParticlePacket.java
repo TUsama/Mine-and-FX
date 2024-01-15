834dbb5f176c36c28417dfeeaeb86d37b5daae45
TUsama <1716976420@qq.com>
2024-01-15 13:59:06 +0800

    change MAS send particle packet strategy

---
+++ src/main/java/com/clefal/mmofx/common/packets/SpellParticlePacket.java
@@ -0,0 +1,67 @@
+package com.clefal.mmofx.common.packets;
+
+import com.clefal.mmofx.ConfigManager;
+import com.robertx22.age_of_exile.mmorpg.SlashRef;
+import com.robertx22.library_of_exile.main.MyPacket;
+import com.robertx22.library_of_exile.packets.ExilePacketContext;
+import com.robertx22.library_of_exile.utils.geometry.MyPosition;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleType;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+
+import static com.clefal.mmofx.common.effect.FXCache.FX_CACHE;
+
+public class SpellParticlePacket extends MyPacket<SpellParticlePacket> {
+    private MyPosition pos;
+    private ParticleOptions particle;
+    private MyPosition vel;
+
+    public SpellParticlePacket(MyPosition pos, ParticleOptions particle, MyPosition vel) {
+        this.pos = pos;
+        this.particle = particle;
+        this.vel = vel;
+    }
+
+    public SpellParticlePacket(){
+    }
+    @Override
+    public ResourceLocation getIdentifier()  {
+        return new ResourceLocation(SlashRef.MODID, "spellparticle");
+    }
+
+    @Override
+    public void loadFromData(FriendlyByteBuf buf) {
+        this.pos = new MyPosition(buf.readDouble(), buf.readDouble(), buf.readDouble());
+        ParticleType<?> particletype = buf.readById(BuiltInRegistries.PARTICLE_TYPE);
+        this.particle = this.readParticle(buf, particletype);
+        this.vel = new MyPosition(buf.readDouble(), buf.readDouble(), buf.readDouble());
+    }
+
+    @Override
+    public void saveToData(FriendlyByteBuf buf) {
+        buf.writeDouble(this.pos.x);
+        buf.writeDouble(this.pos.y);
+        buf.writeDouble(this.pos.z);
+        buf.writeId(BuiltInRegistries.PARTICLE_TYPE, this.particle.getType());
+        buf.writeDouble(this.vel.x);
+        buf.writeDouble(this.vel.y);
+        buf.writeDouble(this.vel.z);
+
+    }
+
+    @Override
+    public void onReceived(ExilePacketContext exilePacketContext) {
+        exilePacketContext.getPlayer().level().addParticle(particle, pos.x, pos.y, pos.z, vel.x, vel.y, vel.z);
+    }
+
+    @Override
+    public MyPacket<SpellParticlePacket> newInstance() {
+        return new SpellParticlePacket();
+    }
+
+    private <T extends ParticleOptions> T readParticle(FriendlyByteBuf buf, ParticleType<T> pt) {
+        return pt.getDeserializer().fromNetwork(pt, buf);
+    }
+}
--- src/main/java/com/clefal/mmofx/mixins/ParticleInRadiusActionMixin.java
+++ src/main/java/com/clefal/mmofx/mixins/ParticleInRadiusActionMixin.java
@@ -1,28 +1,51 @@
 package com.clefal.mmofx.mixins;
 
-import com.robertx22.age_of_exile.database.data.spells.components.MapHolder;
+import com.clefal.mmofx.common.data.FXInfoHolder;
+import com.clefal.mmofx.common.packets.SpellParticlePacket;
 import com.robertx22.age_of_exile.database.data.spells.components.actions.vanity.ParticleInRadiusAction;
+import com.robertx22.age_of_exile.database.data.spells.components.actions.vanity.ParticleMotion;
+import com.robertx22.age_of_exile.database.data.spells.components.actions.vanity.ParticleShape;
 import com.robertx22.age_of_exile.database.data.spells.spell_classes.SpellCtx;
-import net.minecraft.world.entity.LivingEntity;
+import com.robertx22.library_of_exile.main.Packets;
+import com.robertx22.library_of_exile.utils.geometry.MyPosition;
+import com.robertx22.library_of_exile.utils.geometry.ShapeHelper;
+import com.robertx22.library_of_exile.utils.geometry.XTimesData;
+import lombok.val;
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Collection;
+import java.util.stream.Collectors;
 
+import static com.clefal.mmofx.common.data.FXUtilities.getPlayerWithinRange;
 import static com.clefal.mmofx.common.effect.FXCache.FX_CACHE;
 
-@Mixin(ParticleInRadiusAction.class)
-public class ParticleInRadiusActionMixin {
-    @Inject(method = "tryActivate", at = @At(value = "HEAD"), remap = false, cancellable = true)
-    public void injectActivation(Collection<LivingEntity> targets, SpellCtx ctx, MapHolder data, CallbackInfo ci){
-        if(ctx.world.isClientSide){
-            return;
-        }
-        var identifier = ctx.calculatedSpellData.getSpell().identifier;
-        if(FX_CACHE.contains(identifier)){
-            ci.cancel();
-        }
+
+@Mixin(value = ParticleInRadiusAction.class)
+public class ParticleInRadiusActionMixin{
+
+    @Inject(method = "lambda$tryActivate$0",
+            at = @At(value = "INVOKE",
+                    target = "Lcom/robertx22/library_of_exile/utils/geometry/ShapeHelper;spawnParticle(Lnet/minecraft/world/level/Level;Lorg/joml/Vector3d;Lnet/minecraft/core/particles/ParticleOptions;Lorg/joml/Vector3d;)V"),
+            remap = false,
+            cancellable = true
+    )
+    private static void injectActivation(float yrand, ParticleShape shape, Vec3 pos, float finalRadius, Vec3 vel, float height, ParticleMotion finalMotion1, SpellCtx ctx, float motionMulti, ShapeHelper c, SimpleParticleType particle, XTimesData x, CallbackInfo ci){
+        val original = new MyPosition(pos);
+        val nearPlayer = getPlayerWithinRange(pos, ctx.world, 128.0D);
+            nearPlayer.stream()
+                    .collect(Collectors.groupingBy(FXInfoHolder::readFXConfigValue))
+                    .forEach((key, value) -> {
+                        if(!key){
+                            value.forEach(serverPlayer -> Packets.sendToClient(serverPlayer, new SpellParticlePacket(original, particle, new MyPosition(vel))));
+                        }
+                        if(!FX_CACHE.contains(ctx.calculatedSpellData.getSpell().identifier)){
+                            value.forEach(serverPlayer -> Packets.sendToClient(serverPlayer, new SpellParticlePacket(original, particle, new MyPosition(vel))));
+                        }
+                    });
+        ci.cancel();
     }
 }
